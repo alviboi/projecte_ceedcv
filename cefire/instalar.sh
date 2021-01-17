d21e3607fa1ab4607d32ff506b8c3ecae7f3f9b0
alviboi <alviboi@gmail.com>
2021-01-17 13:37:37 +0100

    script

--- cefire/instalar.sh
+++ cefire/instalar.sh
@@ -2,8 +2,8 @@
 cd /var/www/
 php artisan key:generate
 php artisan migrate:fresh
-php composer install
-php composer update
+/var/www/php composer install
+/var/www/php composer update
 npm install
 php artisan db:seed
 npm run prod
--- start.sh
+++ start.sh
@@ -6,4 +6,16 @@ cd ./laradock
  sudo systemctl start docker
 #  
  sudo docker-compose up -d nginx mysql phpmyadmin php-worker
- sudo docker-compose exec workspace bash /var/www/instalar.sh
+
+ sudo docker-compose exec workspace bash /var/www/php artisan key:generate
+ sudo docker-compose exec workspace bash /var/www/php artisan migrate:fresh
+ sudo docker-compose exec workspace bash /var/www//var/www/php composer install
+ sudo docker-compose exec workspace bash /var/www//var/www/php composer update
+ sudo docker-compose exec workspace bash /var/www/npm install
+ sudo docker-compose exec workspace bash /var/www/php artisan db:seed
+ sudo docker-compose exec workspace bash /var/www/npm run prod
+
+
+
+
+ #sudo docker-compose exec workspace bash /var/www/instalar.sh
