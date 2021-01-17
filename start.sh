1fe3eaa48e8b31aca4698eb992deab3e161458ed
alviboi <alviboi@gmail.com>
2021-01-17 13:44:11 +0100

    script

--- start.sh
+++ start.sh
@@ -5,17 +5,17 @@ cd ./laradock
 
  sudo systemctl start docker
 #  
- sudo docker-compose up -d nginx mysql phpmyadmin php-worker
+ sudo docker-compose up -d nginx mysql phpmyadmin php-w /var/wwworker
 
- sudo docker-compose exec workspace bash /var/www/php artisan key:generate
- sudo docker-compose exec workspace bash /var/www/php artisan migrate:fresh
- sudo docker-compose exec workspace bash /var/www//var/www/php composer install
- sudo docker-compose exec workspace bash /var/www//var/www/php composer update
- sudo docker-compose exec workspace bash /var/www/npm install
- sudo docker-compose exec workspace bash /var/www/php artisan db:seed
- sudo docker-compose exec workspace bash /var/www/npm run prod
-
-
+ sudo docker-compose exec -it -w /var/www workspace bash php artisan key:generate
+ sudo docker-compose exec -it -w /var/www workspace bash php artisan migrate:fresh
+ sudo docker-compose exec -it -w /var/www workspace bash php composer install
+ sudo docker-compose exec -it -w /var/www workspace bash php composer update
+ sudo docker-compose exec -it -w /var/www workspace bash npm install
+ sudo docker-compose exec -it -w /var/www workspace bash php artisan db:seed
+ sudo docker-compose exec -it -w /var/www workspace bash npm run prod
 
 
- #sudo docker-compose exec workspace bash /var/www/instalar.sh
+
+
+ #sudo docker-compose exec -it -w /var/www workspace bash instalar.sh
